97e583ea78bfd55cf008101a510096808b092397
Herald <110948069+Resuscitating@users.noreply.github.com>
2023-01-09 16:27:18 -0600

    allow user defined types
    untested but might work, who knows

--- c.py
+++ c.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import builtins as __builtins__
 import sys
 import ctypes
 
 sentinal = object()
+last_vname = ""
 
 typeinfo = {}
 valstorage = sentinal
@@ -14,48 +16,78 @@ cstyle_vars = {}
 class __globals_overload__(dict):
     __slots__ = ()
     
-    def __getitem__(self, item):
+    def __getitem__(self, vname):
+        global typeinfo, valstorage, last_vname
         __getitem__ = {}.__class__.__getitem__
+        if vname is last_vname and valstorage is not sentinal:
+            if last_vname in typeinfo:
+                type_candidate = typeinfo[last_vname]
+                if type(valstorage) is type_candidate:
+                   cstyle_vars[last_vname] = valstorage
+                else:
+                    err = False
+                    try:
+                        casted_value = type_candidate(valstorage)
+                        if type(casted_value) is type_candidate:
+                            cstyle_vars[last_vname] = casted_value
+                        else:
+                            raise TypeError
+                    except:
+                        err = True
+                    if err:
+                        valstorage = sentinal
+                        raise TypeError(f"value '{valstorage}' cannot be cast into type '{type_candidate}'")
+            else:
+                valstorage = sentinal
+                raise TypeError(f"cannot create variable '{last_vname}' without type information")
+            valstorage = sentinal
+
         try:
-            return __getitem__(cstyle_vars, item)
+            return cstyle_vars[vname]
         except:
             try:
-                return __getitem__(self, item)
+                return __getitem__(self, vname)
             except:
                 try:
-                    return __builtins__.__getattribute__(item)
+                    return __builtins__.__getattribute__(vname)
                 except:
                     pass
-        raise NameError(f"{item} is not defined")
+        raise NameError(f"{vname} is not defined")
 
-    def __setitem__(self, item, value):
-        global typeinfo, valstorage
+    def __setitem__(self, vname, value):
+        global typeinfo, valstorage, last_vname
 
         # make import not assign this module into __main__
         if type(value) is type(__builtins__) and value.__name__ == "c":
             return
-
-        # if the variable has already been declared, assign to it
-        if item in typeinfo:
-            if type(value) is typeinfo[item]:
-                cstyle_vars[item] = value
+        
+        if valstorage is not sentinal:
+            if last_vname in typeinfo:
+                type_candidate = typeinfo[last_vname]
+                if type(valstorage) is type_candidate:
+                   cstyle_vars[last_vname] = valstorage
+                else:
+                    err = False
+                    try:
+                        casted_value = type_candidate(valstorage)
+                        if type(casted_value) is type_candidate:
+                            cstyle_vars[last_vname] = casted_value
+                        else:
+                            raise TypeError
+                    except:
+                        err = True
+                    if err:
+                        last_vname = vname
+                        valstorage = value
+                        raise TypeError(f"value '{valstorage}' cannot be cast into type '{type_candidate}'")
             else:
-                err = False
-                try:
-                    casted_value = typeinfo[item](value)
-                    if type(casted_value) is typeinfo[item]:
-                        cstyle_vars[item] = casted_value
-                    else:
-                        raise TypeError
-                except:
-                    err = True
-                if err:
-                    raise TypeError(f"value '{value}' cannot be cast into type '{typeinfo[item]}'")
+                last_vname = vname
+                valstorage = value
+                raise TypeError(f"cannot create variable '{last_vname}' without type information")
+        last_vname = vname
+        valstorage = value
 
-        # store the value so that when __annotations_overload__.__setitem__
-        # gets called, it can use the value to set the variable
-        else:
-            valstorage = value
+
 
 
 def get_main_frame():
@@ -74,39 +106,43 @@ ctypes.py_object.from_address(id(get_main_frame().f_globals) + 8).value = __glob
 class __annotations_overload__(dict):
     __slots__ = ()
 
-    def __setitem__(self, item, value):
+    def __setitem__(self, vtype, vname):
         global typeinfo, valstorage
-        if value not in typeinfo:
+        if vname not in typeinfo:
 
             # find the type
             type_candidate = None
-            if item in cstyle_vars:
-                if type(cstyle_vars[item]) is type:
-                    type_candidate = cstyle_vars[item]
-            if type_candidate is None and item in __builtins__.__dict__:
-                if type(__builtins__.__dict__[item]) is type:
-                    type_candidate = __builtins__.__dict__[item]
+            try:
+                if type(cstyle_vars[vtype]) is type:
+                    type_candidate = cstyle_vars[vtype]
+            except:
+                try:
+                    if type(__builtins__.__dict__[vtype]) is type:
+                        type_candidate = __builtins__.__dict__[vtype]
+                except:
+                    pass
             if type_candidate is None:
-                raise TypeError(f"variable declaration for '{item}: {value}' is not a valid. '{item}' is not a type.")
+                valstorage = sentinal
+                raise TypeError(f"variable declaration for '{vtype}: {vname}' is not a valid. '{vtype}' is not a type.")
 
             # if the value has been stored, set the value
             if valstorage is not sentinal:
                 if type(valstorage) is type_candidate:
-                    cstyle_vars[value] = valstorage
+                    cstyle_vars[vname] = valstorage
                 else:
                     err = False
                     try:
                         casted_value = type_candidate(valstorage)
                         if type(casted_value) is type_candidate:
-                            cstyle_vars[value] = casted_value
+                            cstyle_vars[vname] = casted_value
                         else:
                             raise TypeError
                     except:
                         err = True
                     if err:
-                        raise TypeError(f"value '{valstorage}' cannot be cast into type '{type_candidate}'")
-                valstorage = sentinal
-            typeinfo[value] = type_candidate
+                        raise TypeError(f"value '{(valstorage, valstorage:=sentinal)[0]}' cannot be cast into type '{type_candidate}'")
+            valstorage = sentinal
+            typeinfo[vname] = type_candidate
 
         else:
             raise Exception("something has gone horrifically wrong- tell the author of this module so they can fix this bug!")
@@ -114,4 +150,5 @@ class __annotations_overload__(dict):
 
 # hijack __annotations__
 __import__("__main__").__annotations__ = __annotations_overload__()
+
 
